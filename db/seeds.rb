19ea6ff19bb31967e2281611fa2d6b913c78b6c4
Bhushan Lodha <bhushanlodha@gmail.com>
2017-03-20 16:01:16 +0530

    Added Books model Added faker gem Added seed data for books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+15.times do 
+  Book.create title: Faker::Book.title,
+              description:Faker::Lorem.paragraph,
+              author: Faker::Book.author
+end
