8b9a0d6d931fcbe663857f059f37ee49036bd1ab
Bhushan Lodha <bhushanlodha@gmail.com>
2017-03-21 15:32:59 +0530

    Added seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
+# create books
 15.times do 
-  Book.create title: Faker::Book.title,
-              description:Faker::Lorem.paragraph,
-              author: Faker::Book.author
+  Book.create title:       Faker::Book.title,
+              description: Faker::Lorem.paragraph,
+              author:      Faker::Book.author
+end
+
+# create users
+5.times do |n|
+  User.create! name:  "Foo Bar #{n}",
+              email: "foo#{n}@bar.com",
+              password: "foobar123"
 end
